f4ec01af3c8e07bc325de7d16698aabbf1e17cfc
kopyl <oleggggg1997@gmail.com>
2025-01-19 19:32:11 +0200

    Add backend to the project repo

---
+++ backend/flask-app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from yt_dlp import YoutubeDL
+
+
+app = Flask(__name__)
+    
+
+@app.route('/info', methods=['GET'])
+def info():
+    url = request.args.get('url')
+    if not url:
+        return "Error: No URL provided", 400
+
+    """
+    cookies from Instagram downloaded with
+    https://chromewebstore.google.com/detail/get-cookiestxt-locally/cclelndahbckbenkjhflpdbgdldlbecc
+    """
+    options = {
+        'format': 'bestvideo[vcodec=avc1]+bestaudio[acodec=aac]/mp4',
+        'cookiefile': 'cookies.txt'
+    }
+
+    try:
+        with YoutubeDL(options) as ydl:
+            info_dict = ydl.extract_info(url)
+            print("Info downloaded")
+    except Exception as e:
+        print(f"Download failed: {str(e)}")
+        return f"Error while downloading video: {str(e)}", 500
+
+    return jsonify({"url": info_dict.get("url")})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=6000)
