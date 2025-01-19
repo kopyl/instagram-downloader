798f901d73b9f37285432293f1dd6fa489441b7b
kopyl <oleggggg1997@gmail.com>
2025-01-19 06:21:06 +0200

    Add Instagram URL validation

--- instagram-downloader/ContentView.swift
+++ instagram-downloader/ContentView.swift
@@ -1,6 +1,16 @@
 import SwiftUI
 import AlertKit
 
+func isValidInstagramReelURL(url: String) -> Bool {
+    let pattern = "^https://www\\.instagram\\.com/reel/[A-Za-z0-9_-]+(?:/)?(?:\\?igsh=[A-Za-z0-9=]+)?$"
+    let regex = try! NSRegularExpression(pattern: pattern, options: [])
+    
+    let range = NSRange(location: 0, length: url.utf16.count)
+    let match = regex.firstMatch(in: url, options: [], range: range)
+    
+    return match != nil
+}
+
 struct Notification {
     var windowScene: UIWindow?
     var loadingNotification: AlertAppleMusic17View?
@@ -26,6 +36,7 @@ struct Notification {
 
 struct ContentView: View {
     @Environment(\.scenePhase) var scenePhase
+    @State private var isUrlValid = false
     @State private var url: String = ""
     
     let notification = Notification()
@@ -33,6 +44,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text(url)
+            Text("Is valid: \(String(isUrlValid))")
             Button("Test") {
                 notification.present()
             }
@@ -41,6 +53,8 @@ struct ContentView: View {
             guard scenePhase == .active else { return }
             guard let _url = UIPasteboard.general.string else { return }
             url = _url
+            
+            isUrlValid = isValidInstagramReelURL(url: _url)
         }
     }
 }
