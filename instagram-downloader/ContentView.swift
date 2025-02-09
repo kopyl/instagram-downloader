ec12ff93dbb6827195266d8e803de2ff30f75a60
kopyl <oleggggg1997@gmail.com>
2025-02-10 00:17:08 +0200

    Add data for Xcode Preview

--- instagram-downloader/ContentView.swift
+++ instagram-downloader/ContentView.swift
@@ -338,5 +338,16 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: ReelUrl.self, configurations: config)
+    
+    let context = container.mainContext
+    
+    context.insert(
+        ReelUrl("https://www.instagram.com/reel/DF2k_eFMhgb/?igsh=ZThrZGtta3czcWt5", type: .video)
+    )
+
+    return ContentView()
+        .modelContainer(container)
+        .preferredColorScheme(.dark)
 }
