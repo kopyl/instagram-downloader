8b5fb5323bbbbe0ade9c8df36b14c9f3d2b8e9d5
kopyl <oleggggg1997@gmail.com>
2025-02-14 03:13:21 +0200

    Remove unused code

--- instagram-downloader/Network.swift
+++ instagram-downloader/Network.swift
@@ -29,16 +29,6 @@ func downloadFile(from url: _URL) async throws -> URL? {
     return destinationURL
 }
 
-func isValidInstagramReelURL(url: String) -> Bool {
-    let pattern = "^https://(www.)?instagram\\.com/(reel|p)/[A-Za-z0-9_-]+(?:/)?(?:\\?igsh=[A-Za-z0-9%=]+)?$"
-    let regex = try! NSRegularExpression(pattern: pattern, options: [])
-    
-    let range = NSRange(location: 0, length: url.utf16.count)
-    let match = regex.firstMatch(in: url, options: [], range: range)
-    
-    return match != nil
-}
-
 func urlToVideoCode(_ url: String) -> String? {
     guard let urlComponents = URL(string: url) else { return nil }
     let path = urlComponents.path
