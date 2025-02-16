335f841747eec59d86bea53cc17af76541048c10
kopyl <oleggggg1997@gmail.com>
2025-02-16 19:40:26 +0200

    Refactor

---
+++ Buttons.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct LoginBrowserButton: View {
+    var action: () -> Void
+    
+    init(action: @escaping () -> Void) {
+        self.action = action
+    }
+    
+    var body: some View {
+        Button(action: action) {
+            Image(systemName: "safari")
+                .font(.title2)
+                .foregroundColor(.white)
+                .padding()
+                .opacity(0.7)
+        }
+    }
+}
+
+struct CopyButton: View {
+    let text: String?
+    let reelUrl: ReelUrl
+    let notification: Notification
+    
+    var body: some View {
+        Button {
+            UIPasteboard.general.string = reelUrl.cleanURL()
+//            notification.present(type: .success, title: "URL copied")
+        } label: {
+            if let text {
+                Text(text)
+            }
+            Image(systemName: "document.on.document.fill")
+        }
+    }
+}
---
+++ Utils.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+func formattedDate(_ date: Date) -> String {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.unitsStyle = .short
+        return formatter.localizedString(for: date, relativeTo: Date())
+    }
