88dcda577a4e51feac9a387a0d9490212362d0f7
kopyl <oleggggg1997@gmail.com>
2025-02-26 23:46:56 +0200

    Reset cookies each WebView appears
    Instagram may sometimes ask users to confirm their account with a code from their email and if there is a persistent cookie in the WebView browser, then instagram may not load the login page for some reason, so i figured out the best way to deal this so far is to clear out the cookies before showing the Instagram login webview.
    
    Although there is a bug where a user is required to press "share" again, while I set up the logic in a way that re-extracting shared items should happen automatically after a user dismissed the Instagram login WebView sheet with a correct credentials.

--- instagram-downloader/WebView.swift
+++ instagram-downloader/WebView.swift
@@ -43,12 +43,36 @@ struct WebView: UIViewRepresentable {
     }
 
     func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
+        // Create a new configuration for the WebView
+        let configuration = WKWebViewConfiguration()
+        
+        // Create a non-persistent data store (this will not save data between app launches)
+        let nonPersistentDataStore = WKWebsiteDataStore.nonPersistent()
+        configuration.websiteDataStore = nonPersistentDataStore
+        
+        // Create the WebView with the configuration
+        let webView = WKWebView(frame: .zero, configuration: configuration)
         webView.navigationDelegate = context.coordinator
+        
+        // Create and load the request
         let request = URLRequest(url: url)
         webView.load(request)
+        
         return webView
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {}
+    
+    // Helper method to clear cookies
+    private func clearCookies(in webView: WKWebView) {
+        // Get all cookie types
+        let dataTypes = WKWebsiteDataStore.allWebsiteDataTypes()
+        
+        // Clear all website data
+        webView.configuration.websiteDataStore.removeData(
+            ofTypes: dataTypes,
+            modifiedSince: Date(timeIntervalSince1970: 0),
+            completionHandler: {}
+        )
+    }
 }
