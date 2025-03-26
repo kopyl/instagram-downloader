df6020dc284dcf7526e8ec99ab1fe2d1d2836575
kopyl <oleggggg1997@gmail.com>
2025-03-26 06:49:05 +0200

    Move thumbnails storing to file system

--- Data.swift
+++ Data.swift
@@ -8,7 +8,7 @@ class Reel {
     var type: URLTypes
     var dateSaved: Date
     
-    @Attribute(.externalStorage) var thumbnailData: Data?
+    var thumbnailPath: String?
 
     init(_ url: String, type: URLTypes, mediaIdentifierFromPhotosApp: String? = nil) {
         self.url = url
@@ -23,11 +23,45 @@ class Reel {
     
     var thumbnail: UIImage? {
         get {
-            guard let data = thumbnailData else { return nil }
-            return UIImage(data: data)
+            guard let path = thumbnailPath else {
+                return nil
+            }
+            guard let appGroupDirectory = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: Names.APPGROUP) else {
+                return nil
+            }
+            let directory = appGroupDirectory.appendingPathComponent("Application Support")
+            let fileName = directory.appendingPathComponent(path)
+            return UIImage(contentsOfFile: fileName.path)
         }
         set {
-            thumbnailData = newValue?.pngData()
+            if let newValue = newValue {
+                thumbnailPath = saveThumbnailToDisk(newValue)
+            }
+        }
+    }
+    
+    private func saveThumbnailToDisk(_ image: UIImage) -> String? {
+        guard let data = image.pngData() else { return nil }
+        
+        guard let appGroupDirectory = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: Names.APPGROUP) else {
+            return nil
+        }
+        
+        let directory = appGroupDirectory.appendingPathComponent("Application Support")
+        
+        let fileManager = FileManager.default
+        if !fileManager.fileExists(atPath: directory.path) {
+            try? fileManager.createDirectory(at: directory, withIntermediateDirectories: true)
+        }
+        
+        let fileName = UUID().uuidString + ".png"
+        let filePath = directory.appendingPathComponent(fileName)
+        
+        do {
+            try data.write(to: filePath)
+            return fileName
+        } catch {
+            return nil
         }
     }
 }
